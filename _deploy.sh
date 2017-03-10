2951f4c750f32a774c28ad18d7fbc03164cdc852
Akira Matsui <akira@ip195-139.wlan.kobe-u.ac.jp>
2017-03-10 17:13:03 +0900

    add theme

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,9 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
+echo ${TRAVIS_BRANCH}
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
 
 git config --global user.email "akira00120@gmail.com"
 git config --global user.name "Akira Matsui "
@@ -12,5 +14,5 @@ git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git sit
 cd site-output
 cp -r ../public/* ./
 git add --all *
-git commit -m"Update the site" || true
+git commit -m　"Update the site" || true
 git push -q origin master
