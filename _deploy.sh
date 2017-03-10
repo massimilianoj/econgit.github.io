8735548762d8f0c32ff8afe637c7f2bc308f1375
Akira Matsui <akira@ip195-139.wlan.kobe-u.ac.jp>
2017-03-10 17:31:19 +0900

    fix a git command

--- _deploy.sh
+++ _deploy.sh
@@ -4,13 +4,14 @@ set -e
 
 
 #[ -z "${GITHUB_PAT}" ] && exit 0
-echo ${TRAVIS_BRANCH}
-#[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 
 git config --global user.email "akira00120@gmail.com"
 git config --global user.name "Akira Matsui "
 
+echo https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git site-output
 git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git site-output
 cd site-output
 cp -r ../public/* ./
