674589f65a247235d3343fa42830e86f3f72c481
Akira Matsui <akira@ip195-139.wlan.kobe-u.ac.jp>
2017-03-10 17:06:41 +0900

    fix a git command

--- _deploy.sh
+++ _deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 git config --global user.email "akira00120@gmail.com"
 git config --global user.name "Akira Matsui "
