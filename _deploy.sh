35bf044a573438d8c766c3ef57511baf4efcc1b4
Akira Matsui <akira@ip195-139.wlan.kobe-u.ac.jp>
2017-03-11 10:33:15 +0900

    rewrite script

--- _deploy.sh
+++ _deploy.sh
@@ -2,21 +2,15 @@
 
 set -e
 
-#echo ${TRAVIS_BRANCH}
-#echo "TEST0"
-#[ -z "${GITHUB_PAT}" ] && exit 0
-#echo "TEST1"
+[ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "source" ] && exit 0
-echo "TEST2"
-
 
 git config --global user.email "akira00120@gmail.com"
-git config --global user.name "Akira Matsui "
+git config --global user.name "Akira Matsui"
 
-echo https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git site-output
 git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git site-output
 cd site-output
 cp -r ../public/* ./
 git add --all *
-git commit -m　"Update the site" || true
+git commit -m"Update the site" || true
 git push -q origin master
